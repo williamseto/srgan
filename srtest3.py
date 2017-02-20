4ee923b3725cfd797be11517ac7046f316e21974
William Seto <williamseto2@gmail.com>
2017-02-19 20:41:17 -0500

    replace batch norm

--- srtest3.py
+++ srtest3.py
@@ -29,11 +29,11 @@ def print_shape(t):
 #     opt_saver = tf.train.Saver(restore_vars)
 #     opt_saver.restore(session, save_file)
 
-# def lrelu(x, leak=0.2, name="lrelu"):
-#     with tf.variable_scope(name):
-#         f1 = 0.5 * (1 + leak)
-#         f2 = 0.5 * (1 - leak)
-#         return f1 * x + f2 * abs(x)
+def lrelu(x, leak=0.2, name="lrelu"):
+    with tf.variable_scope(name):
+        f1 = 0.5 * (1 + leak)
+        f2 = 0.5 * (1 - leak)
+        return f1 * x + f2 * abs(x)
 
 # def residual(inputs, name="res"):
 #     with tf.variable_scope(name) as scope:
@@ -88,7 +88,7 @@ class batch_norm(object):
 
 # this is really bad
 batch_norm_list = []
-nb_residual = 15
+nb_residual = 10
 n_extra_bn = 1
 for n in range(nb_residual*2 + n_extra_bn):
     batch_norm_list.append(batch_norm(name='bn'+str(n)))
@@ -101,9 +101,9 @@ for n in range(nb_residual*2 + n_extra_bn):
 adam_learning_rate = 0.0001
 adam_beta1 = 0.9
 
-batch_size = 128
-image_h = 64
-image_w = 64
+batch_size = 32
+image_h = 128
+image_w = 128
 
 num_epochs = 20
 
@@ -168,30 +168,114 @@ with tf.variable_scope("generator") as scope:
     gen = create_generator(inputs)
     scope.reuse_variables()
     gen_test = create_generator(inputs, False)
-    
+
+
+print "DISCRIMINATOR"
+print "--------------"
+#disc_bn1 = batch_norm(name='d_bn1')
+disc_bn2 = batch_norm(name='d_bn2')
+disc_bn3 = batch_norm(name='d_bn3')
+disc_bn4 = batch_norm(name='d_bn4')
+disc_bn5 = batch_norm(name='d_bn5')
+def create_discriminator(inputs):
+    with slim.arg_scope([slim.conv2d],
+                    padding='SAME',
+                    activation_fn=None,
+                    # activation_fn=lrelu,
+                    # normalizer_fn=slim.batch_norm,
+                    weights_initializer=tf.truncated_normal_initializer(stddev=0.02),
+                    weights_regularizer=slim.l2_regularizer(0.0005),
+                    stride=2):
+
+        noisy_inputs = inputs + tf.random_normal(shape=tf.shape(inputs), mean=0.0, stddev=0.05, dtype=tf.float32)
+
+        # disc = slim.conv2d(noisy_inputs, 64, [5, 5], scope='conv1')
+        # print_shape(disc)
+
+        # disc = slim.conv2d(disc, 128, [5, 5], scope='conv2')
+        # print_shape(disc)
+
+        # disc = slim.conv2d(disc, 256, [5, 5], scope='conv3')
+        # print_shape(disc)
+
+        # disc = slim.conv2d(disc, 512, [5, 5], scope='conv4')
+        # print_shape(disc)
+
+        # disc = slim.conv2d(disc, 512, [5, 5], scope='conv5')
+        # print_shape(disc)
+        disc = lrelu(slim.conv2d(noisy_inputs, 64, [5, 5], scope='conv1'))
+        print_shape(disc)
+
+        disc = lrelu(disc_bn2(slim.conv2d(disc, 128, [5, 5], scope='conv2')))
+        print_shape(disc)
+
+        disc = lrelu(disc_bn3(slim.conv2d(disc, 256, [5, 5], scope='conv3')))
+        print_shape(disc)
+
+        disc = lrelu(disc_bn4(slim.conv2d(disc, 512, [5, 5], scope='conv4')))
+        print_shape(disc)
+
+        disc = lrelu(disc_bn5(slim.conv2d(disc, 512, [5, 5], scope='conv5')))
+        print_shape(disc)
+
+    disc_logits = slim.fully_connected(tf.contrib.layers.flatten(disc), 1, activation_fn=None, scope='fc6')
+    return disc_logits
+# create 2 discriminators: for fake and real images
+with tf.variable_scope("discriminators") as scope:
+    disc_real = create_discriminator(real_ims)
+    scope.reuse_variables()
+    disc_fake = create_discriminator(gen)
 
 
 # Losses
 ###########
 
+# loss on real input images; all outputs should be 1
+
+# make labels noisy
+if np.random.rand() < 0.8:
+    labels_real = tf.ones_like(disc_real)
+    labels_fake = tf.zeros_like(disc_fake)
+else:
+    labels_real = tf.zeros_like(disc_real)
+    labels_fake = tf.ones_like(disc_fake)
+
+d_loss_real = tf.reduce_mean( \
+    tf.nn.sigmoid_cross_entropy_with_logits(disc_real, labels_real))
+
+# loss on fake input images, fakes should be 0
+d_loss_fake = tf.reduce_mean( \
+    tf.nn.sigmoid_cross_entropy_with_logits(disc_fake, labels_fake))
+
+# similar to above, but we want fake (generator) images to output 1
+g_loss_adv = tf.reduce_mean( \
+    tf.nn.sigmoid_cross_entropy_with_logits(disc_fake, tf.ones_like(disc_fake)))
+
 
 # L2 LOSS
 
-diff_mse = tf.contrib.layers.flatten(gen - real_ims)
+# pretend we don't have ground truth so all we can do is 
+# do reconstruction loss on the input
+outputs = tf.image.resize_images(gen, [image_h/4, image_w/4])
+
+diff_mse = tf.contrib.layers.flatten(outputs - inputs)
 sum_mse = tf.reduce_sum(tf.square(diff_mse), 1)
-g_loss_L2 = tf.reduce_mean(sum_mse)
+g_loss_L2 = tf.reduce_mean(sum_mse/(3*(image_h/4)*(image_h/4)))
 
+g_loss = g_loss_L2 + 0.05*g_loss_adv
 
-g_loss = g_loss_L2
+d_loss = d_loss_real + d_loss_fake
 
 
 
 train_vars = tf.trainable_variables()
-
+d_vars = [var for var in train_vars if 'discriminators' in var.name]
 g_vars = [var for var in train_vars if 'generator' in var.name]
 
 
-# optimize the generator
+# optimize the generator and discriminator separately
+d_optim = tf.train.AdamOptimizer(adam_learning_rate, beta1=adam_beta1) \
+                  .minimize(d_loss, var_list=d_vars)
 g_optim = tf.train.AdamOptimizer(adam_learning_rate, beta1=adam_beta1) \
                   .minimize(g_loss, var_list=g_vars)
     
@@ -204,10 +288,10 @@ print "initialization done"
 # TRAINING
 ############
 
-#data_dir = '/home/wseto/datasets/img_align_celeba128'
-data_dir = '/home/wseto/datasets/wiki_align'
+data_dir = '/home/wseto/datasets/img_align_celeba128'
+#data_dir = '/home/wseto/datasets/wiki_align'
 data = glob(os.path.join(data_dir, "*.png"))
-data_train, data_test = train_test_split(data, test_size=0.1, random_state=42)
+data_train, data_test = train_test_split(data, test_size=0.2, random_state=42)
 
 
 print "TRAINING"
@@ -249,27 +333,44 @@ with tf.Session(config=config) as sess:
             batch_inputs = [np.array(im.resize((image_h/4, image_w/4), Image.BICUBIC)) for im in batch]
             batch_inputs = np.array(batch_inputs).astype(np.float32)/127.5-1
 
+            rand_idx = np.random.randint(len(data_test)-batch_size-1)
+            disc_batch_files = data_test[rand_idx: rand_idx+batch_size]
+            disc_batch = [imread(path) for path in disc_batch_files]
+            disc_batch_images = np.array(disc_batch).astype(np.float32)/127.5-1
+
             #pdb.set_trace()
+            sess.run([d_optim], feed_dict={ inputs: batch_inputs, real_ims: disc_batch_images})
 
 
             # Update G network
-            sess.run([g_optim], feed_dict={ inputs: batch_inputs, real_ims: batch_images})
+            sess.run([g_optim], feed_dict={ inputs: batch_inputs, real_ims: disc_batch_images})
+            # Run g_optim twice to make sure that d_loss does not go to zero
+            sess.run([g_optim], feed_dict={ inputs: batch_inputs, real_ims: disc_batch_images})
 
-            errG = g_loss.eval({ inputs: batch_inputs, real_ims: batch_images})
+            errD_fake = d_loss_fake.eval({inputs: batch_inputs})
+            errD_real = d_loss_real.eval({real_ims: disc_batch_images})
+
+
+            errG = g_loss.eval({ inputs: batch_inputs, real_ims: disc_batch_images})
 
             counter += 1
-            print("Epoch: [%2d] [%4d/%4d] time: %4.4f, g_loss: %.8f" \
-                % (epoch, idx, num_batches, time.time() - start_time, errG))
+            print("Epoch: [%2d] [%4d/%4d] time: %4.4f, d_loss: %.8f, g_loss: %.8f" \
+                % (epoch, idx, num_batches,
+                    time.time() - start_time, errD_fake+errD_real, errG))
 
             # every 500 steps, save some images to see performance of network
-            if np.mod(counter, 25) == 1:
+            if np.mod(counter, 500) == 1:
 
                 rand_idx = np.random.randint(len(data_train))
                 sample_image_orig = Image.open(data_train[rand_idx])
                 # looks nasty, but we are making it 4D
                 sample_image = np.array([np.array(sample_image_orig.resize((image_h/4, image_w/4), Image.BICUBIC))]).astype(np.float32)
-                
-                sample = sess.run([gen_test], feed_dict={inputs: (sample_image/127.5-1)})
+                sample_input = sample_image/127.5-1
+                real_input = (np.array([np.array(sample_image_orig)]))/127.5-1
+                #sample = sess.run([gen_test], feed_dict={inputs: (sample_image/127.5-1)})
+                sample, loss = sess.run([gen_test, g_loss], feed_dict={inputs: sample_input, real_ims: real_input})
+                print "Sample loss: ", loss
+                sample = [sample]
 
                 # save an image, with the original next to the generated one
                 resz_input = sample_image[0].repeat(axis=0,repeats=4).repeat(axis=1,repeats=4)
@@ -280,7 +381,7 @@ with tf.Session(config=config) as sess:
                 imsave('./samples/train_{:02d}_{:04d}.png'.format(epoch, idx), merge_im)
 
 
-            # if np.mod(counter, 1000) == 2:
-            #     weight_saver.save(sess, 'checkpoint/model', counter)
-            #     print "saving a checkpoint"
+            if np.mod(counter, 1000) == 2:
+                weight_saver.save(sess, 'checkpoint_bn/model', counter)
+                print "saving a checkpoint"
 
